102bc3bbf2947d7ca97febd69524024fbc3733a7
pytho <pythorevolution@gmail.com>
2020-12-19 11:00:43 +0800

    remove toast on App.java

--- app/src/main/java/com/exzray/ofoodvendor/App.java
+++ app/src/main/java/com/exzray/ofoodvendor/App.java
@@ -8,6 +8,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
+
     }
 }
